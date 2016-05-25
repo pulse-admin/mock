2b76afd6c8f1153b498d203f50eea71568fc6c1c
Katy Ekey <kekey@ainq.com>
2016-05-25 09:25:38 -0400

    made organizations file reference the classpath, removed .classpath

--- src/main/java/gov/ca/emsa/controller/DirectoryController.java
+++ src/main/java/gov/ca/emsa/controller/DirectoryController.java
@@ -1,8 +1,12 @@
 package gov.ca.emsa.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,11 +19,20 @@ import io.swagger.annotations.Api;
 @RestController
 @Api(value="/mock/directory")
 public class DirectoryController {
-	private String xmlLocation = "C:/Users/blindsey/workspace/pulse/mock/src/main/resources/organizations1.xml";
-	@Autowired
-	private ResourceLoader resourceLoader;
+	private static final String RESOURCE_FILE_NAME = "organizations.xml";
+	private static final Logger logger = LogManager.getLogger(DirectoryController.class);
+	@Autowired private ResourceLoader resourceLoader;
+	
 	@RequestMapping(value= "/mock/directory", method = RequestMethod.GET, produces="application/json; charset=utf-8")
 	public List<Organization> directory() {
-    	return Utils.readOrganizations(xmlLocation);
-    }
+		Resource documentsFile = resourceLoader.getResource("classpath:" + RESOURCE_FILE_NAME);
+		
+		List<Organization> results = null;
+		try {
+			results = Utils.readOrganizations(documentsFile.getFile().getAbsolutePath());
+		} catch(IOException ioEx) {
+			logger.error("Could not read documents file " + documentsFile.getFilename(), ioEx);
+		}
+		return results;
+	}
 }
