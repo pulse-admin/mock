e810dfda52ead2c5cb7bb0fb0174fc42ed53180b
Katy Ekey <kekey@ainq.com>
2016-11-25 15:19:19 -0500

    PULSE-273: add unit tests and parsing for cten objs

--- src/main/java/gov/ca/emsa/controller/DirectoryController.java
+++ src/main/java/gov/ca/emsa/controller/DirectoryController.java
@@ -12,52 +12,64 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import gov.ca.emsa.Utils;
-import gov.ca.emsa.domain.Organization;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gov.ca.emsa.pulse.cten.domain.Endpoint;
+import gov.ca.emsa.pulse.cten.domain.EndpointWrapper;
+import gov.ca.emsa.pulse.cten.domain.Location;
+import gov.ca.emsa.pulse.cten.domain.LocationWrapper;
+import gov.ca.emsa.pulse.cten.domain.Organization;
+import gov.ca.emsa.pulse.cten.domain.OrganizationWrapper;
 import io.swagger.annotations.Api;
 
 @RestController
 @Api(value="/mock")
 public class DirectoryController {
+	private static final Logger logger = LogManager.getLogger(DirectoryController.class);
 	private static final String ORGANIZATION_RESOURCE_FILE_NAME = "organizations.json";
 	private static final String LOCATIONS_RESOURCE_FILE_NAME = "locations.json";
 	private static final String ENDPOINTS_RESOURCE_FILE_NAME = "endpoints.json";
-	private static final Logger logger = LogManager.getLogger(DirectoryController.class);
+
 	@Autowired private ResourceLoader resourceLoader;
+	private ObjectMapper jsonMapper;
+	
+	public DirectoryController() {
+		jsonMapper = new ObjectMapper();
+	}
 	
 	@RequestMapping(value= "/Organization", method = RequestMethod.GET, produces="application/json; charset=utf-8")
 	public List<Organization> getOrganizations() {
 		Resource organizationsFile = resourceLoader.getResource("classpath:" + ORGANIZATION_RESOURCE_FILE_NAME);
-		List<Organization> results = null;
+		OrganizationWrapper parsed = null;
 		try {
-			results = Utils.readOrganizations(organizationsFile.getInputStream());
-		} catch(IOException ioEx) {
-			logger.error("Could not read organizations file " + organizationsFile.getFilename(), ioEx);
+			parsed = jsonMapper.readValue(organizationsFile.getInputStream(), OrganizationWrapper.class);
+		} catch(IOException ex) {
+			logger.error("Could not parse organizations file", ex);
 		}
-		return results;
+		return parsed.getEntry();
 	}
 	
 	@RequestMapping(value= "/Location", method = RequestMethod.GET, produces="application/json; charset=utf-8")
-	public List<Organization> getLocations() {
+	public List<Location> getLocations() {
 		Resource locationsFile = resourceLoader.getResource("classpath:" + LOCATIONS_RESOURCE_FILE_NAME);
-		List<Organization> results = null;
+		LocationWrapper parsed = null;
 		try {
-			results = Utils.readOrganizations(locationsFile.getInputStream());
-		} catch(IOException ioEx) {
-			logger.error("Could not read organizations file " + locationsFile.getFilename(), ioEx);
+			parsed = jsonMapper.readValue(locationsFile.getInputStream(), LocationWrapper.class);
+		} catch(IOException ex) {
+			logger.error("Could not parse locations file", ex);
 		}
-		return results;
+		return parsed.getEntry();
 	}
 	
 	@RequestMapping(value= "/Endpoint", method = RequestMethod.GET, produces="application/json; charset=utf-8")
-	public List<Organization> getEndpoints() {
+	public List<Endpoint> getEndpoints() {
 		Resource endpointsFile = resourceLoader.getResource("classpath:" + ENDPOINTS_RESOURCE_FILE_NAME);
-		List<Organization> results = null;
+		EndpointWrapper parsed = null;
 		try {
-			results = Utils.readOrganizations(endpointsFile.getInputStream());
-		} catch(IOException ioEx) {
-			logger.error("Could not read organizations file " + endpointsFile.getFilename(), ioEx);
+			parsed = jsonMapper.readValue(endpointsFile.getInputStream(), EndpointWrapper.class);
+		} catch(IOException ex) {
+			logger.error("Could not parse endpoints file", ex);
 		}
-		return results;
+		return parsed.getEntry();
 	}
 }
--- src/test/java/gov/ca/emsa/DirectoryControllerTests.java
+++ src/test/java/gov/ca/emsa/DirectoryControllerTests.java
@@ -1,56 +1,108 @@
 package gov.ca.emsa;
 
-import gov.ca.emsa.controller.DirectoryController;
+import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.WebMvcProperties.View;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.MediaType;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gov.ca.emsa.pulse.cten.domain.Endpoint;
+import gov.ca.emsa.pulse.cten.domain.EndpointResource;
+import gov.ca.emsa.pulse.cten.domain.EndpointWrapper;
+import gov.ca.emsa.pulse.cten.domain.Location;
+import gov.ca.emsa.pulse.cten.domain.LocationResource;
+import gov.ca.emsa.pulse.cten.domain.LocationWrapper;
+import gov.ca.emsa.pulse.cten.domain.Organization;
+import gov.ca.emsa.pulse.cten.domain.OrganizationResource;
+import gov.ca.emsa.pulse.cten.domain.OrganizationWrapper;
+import junit.framework.TestCase;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = MockTestConfig.class)
-public class DirectoryControllerTests{
+public class DirectoryControllerTests extends TestCase {
+	@Autowired private ResourceLoader resourceLoader;
+	ObjectMapper jsonMapper;
 	
-	@Mock DirectoryController dc;
-	
-	MockMvc mvc;
-
+	@Before
 	public void setup() {
-		 MockitoAnnotations.initMocks(this);
-		 mvc = MockMvcBuilders.standaloneSetup(dc).build();
+		 jsonMapper = new ObjectMapper();
 	}
 	
 	@Test
-	public void getDirectoriesTest() throws Exception{
-		setup();
-		mvc.perform(get("/mock/directory"))
-		.andExpect(status().isOk())
-		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+	public void parseOrganizations() {
+		OrganizationWrapper parsed = null;
+		Resource orgsResource = resourceLoader.getResource("classpath:organizations.json");
+		try {
+			parsed = jsonMapper.readValue(orgsResource.getInputStream(), OrganizationWrapper.class);
+		} catch(IOException ex) {
+			ex.printStackTrace();
+			fail("Caught IOException parsing organizations: " + ex.getMessage());
+		}
+		
+		assertNotNull(parsed);
+		assertEquals(3, parsed.getTotal().intValue());
+		Organization firstOrg = parsed.getEntry().get(0);
+		assertEquals("http://localhost:9080/mock/Organization/1", firstOrg.getFullUrl());
+		OrganizationResource resource = firstOrg.getResource();
+		assertEquals("John Muir Health Foundation", resource.getName());
+		assertEquals("1", resource.getId());
+		assertEquals("true", resource.getActive());
 	}
 	
 	@Test
-	public void getDirectoriesNoneTest() throws Exception{
-		setup();
-		mvc.perform(get("/mock/directory"))
-		.andExpect(status().isOk())
-		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-		.andExpect(content().json("[]"));
+	public void parseLocations() {
+		LocationWrapper parsed = null;
+		Resource locationsResource = resourceLoader.getResource("classpath:locations.json");
+		try {
+			parsed = jsonMapper.readValue(locationsResource.getInputStream(), LocationWrapper.class);
+		} catch(IOException ex) {
+			ex.printStackTrace();
+			fail("Caught IOException parsing locations: " + ex.getMessage());
+		}
+		
+		assertNotNull(parsed);
+		assertEquals(4, parsed.getTotal().intValue());
+		Location firstLocation = parsed.getEntry().get(0);
+		assertEquals("https://localhost:9080/mock/Location/1", firstLocation.getFullUrl());
+		LocationResource resource = firstLocation.getResource();
+		assertNotNull(resource.getEndpoint());
+		assertEquals(5, resource.getEndpoint().size());
+		assertEquals("John Muir Medical Center", resource.getName());
+		assertNotNull(resource.getManagingOrganization());
+		assertEquals("John Muir Health Foundation", resource.getManagingOrganization().getDisplay());
+		assertEquals("1", resource.getId());
+		assertEquals("active", resource.getStatus());
 	}
+	
+	@Test
+	public void parseEndpoints() {
+		EndpointWrapper parsed = null;
+		Resource endpointsResource = resourceLoader.getResource("classpath:endpoints.json");
+		try {
+			parsed = jsonMapper.readValue(endpointsResource.getInputStream(), EndpointWrapper.class);
+		} catch(IOException ex) {
+			ex.printStackTrace();
+			fail("Caught IOException parsing endpoints: " + ex.getMessage());
+		}
+		
+		assertNotNull(parsed);
+		assertEquals(13, parsed.getTotal().intValue());
+		Endpoint firstEndpoint = parsed.getEntry().get(0);
+		assertEquals("http://localhost:9080/mock/Endpoint/1", firstEndpoint.getFullUrl());
+		EndpointResource resource = firstEndpoint.getResource();
+		assertEquals("1", resource.getId());
+		assertEquals("application/xml", resource.getPayloadFormat());
+		assertEquals("active", resource.getStatus());
+		assertEquals("http://localhost:9080/mock/patientDiscovery", resource.getAddress());
+	}
+	
 }
